92aabfdb481d79c20d7f0d7212945bad24512eab
saikiWindow <nasi.goreng1730@gmail.com>
2016-09-28 00:55:37 +0900

    無限スクロールメイン画面でしかしないように

--- wp-content/themes/gridster-lite/inc/template-tags.php
+++ wp-content/themes/gridster-lite/inc/template-tags.php
@@ -41,7 +41,7 @@ function gridster_content_nav( $nav_id ) {
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
 		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'gridster-lite' ) ); ?></div>
+		<div class="nav-previous-main"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'gridster-lite' ) ); ?></div>
 		<?php endif; ?>
 
 		<?php if ( get_previous_posts_link() ) : ?>
