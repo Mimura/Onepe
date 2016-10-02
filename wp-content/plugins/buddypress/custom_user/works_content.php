c1ca4f24cfd392af03cb89b81bea7f27c1cfc70b
saikiWindow <nasi.goreng1730@gmail.com>
2016-10-02 14:23:21 +0900

    プロフィールページの投稿一覧レイアウト修正

--- wp-content/plugins/buddypress/custom_user/works_content.php
+++ wp-content/plugins/buddypress/custom_user/works_content.php
@@ -6,11 +6,14 @@ $user_id = $bp->displayed_user->id;
 <?php $paged = get_query_var('paged'); ?>
 <!--query_postsで、author=ユーザーID　で現在表示中のユーザーの記事一覧が取得可能-->
 <?php query_posts("author={$user_id}&posts_per_page=10&cat=0&paged={$paged}"); ?>
+		<div id="content" class="site-content archive" role="main">
 <?php while(have_posts()): the_post();
     //include TEMPLATEPATH . 'template-name.php';
     include(get_theme_root() . '/gk-portfolio/content-archive.php');
     //get_template_part( 'content-archive', get_post_format() );
     ?>
 <?php endwhile; ?>
+
+</div>
 <?php wp_reset_query(); ?>
 
