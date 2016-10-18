11019550287a6c4865f1045a8a0612c8a6da4f7e
s-iijima <s-iijima@s-iijima-MB365.local>
2016-10-18 23:23:03 +0900

    無駄な記述を削除

--- wp-content/plugins/buddypress/custom_user/works_content.php
+++ wp-content/plugins/buddypress/custom_user/works_content.php
@@ -5,7 +5,7 @@ $user_id = $bp->displayed_user->id;
 ?>
 <?php $paged = get_query_var('paged'); ?>
 <!--query_postsで、author=ユーザーID　で現在表示中のユーザーの記事一覧が取得可能-->
-<?php query_posts("author={$user_id}&posts_per_page=10&cat=0&paged={$paged}"); ?>
+<?php query_posts("author={$user_id}&paged={$paged}"); ?>
 <div id="content" class="site-content archive" role="main">
     <?php while(have_posts()): the_post();
         //include TEMPLATEPATH . 'template-name.php';
